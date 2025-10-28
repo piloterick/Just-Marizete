d44214d2edffc31005364d94a0ee5b5371bf65be
Erick Costa <deoliveira.erick@gmail.com>
2025-10-28 01:12:36 +0100

    carrello spesa

--- js/script.js
+++ js/script.js
@@ -8,3 +8,54 @@ function menuShow() {
         document.querySelector('.icona-menu').src = "sources/images/close-menu.png";
     }
 }
+
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const cartClose = document.querySelector("#cart-close");
+cartIcon.addEventListener("click", () => cart.classList.add("active"));
+cartClose.addEventListener("click", () => cart.classList.remove("active"));
+
+const addCartButtons = document.querySelectorAll(".shopping-bag");
+addCartButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        const productBox = event.target.closest(".card");
+        addToCart(productBox);
+    });
+});
+
+const cartContent = document.querySelector(".cart-content");
+const addToCart = card => {
+    const productImgSrc = card.querySelector("img").src;
+    const productTitle = card.querySelector(".product-title").textContent;
+    const productPrice = card.querySelector(".price").textContent;
+
+    const cartItems = cartContent.querySelectorAll(".cart-product-title");
+    for ( let item of cartItems) {
+        if (item.textContent === productTitle) {
+            alert("Questo prodotto è già nel carrello.");
+            return;
+        }
+    }
+
+    const cartBox = document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML = `
+        <img src="${productImgSrc}" alt="foto prodotto" class="cart-img">
+                <div class="cart-detail">
+                    <h2 class="cart-product-title">${productTitle}</h2>
+                    <span class="cart-price">${productPrice}</span>
+                    <div class="cart-quantity">
+                        <button id="decrement">-</button>
+                        <span class="number">1</span>
+                        <button id="increment">+</button>
+                    </div>
+                </div>
+                <i class="ri-delete-bin-line cart-remove"></i>
+    `;
+
+    cartContent.appendChild(cartBox);
+
+    cartBox.querySelector(".cart-remove").addEventListener("click", () => {
+        cartBox.remove();
+    });
+};
