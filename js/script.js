87eebed3cf1936c160ce3dfde245d6ad0db4ae98
Erick Costa <deoliveira.erick@gmail.com>
2025-10-28 15:23:47 +0100

    listClose function

--- js/script.js
+++ js/script.js
@@ -7,7 +7,19 @@ function menuShow() {
         menuMobile.classList.add('open');
         document.querySelector('.icona-menu').src = "sources/images/close-menu.png";
     }
-}
+};
+
+function listClose() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icona-menu').src = "sources/images/menu-list.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icona-menu').src = "sources/images/close-menu.png";
+    }
+};
+
 
 const cartIcon = document.querySelector("#cart-icon");
 const cart = document.querySelector(".cart");
